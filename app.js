a033c99a5fec851e2f36272d77c4e50c44f686eb
wadjakorn <wadjakorn@dollarsign.co>
2019-06-23 22:49:58 +0700

    first

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const http = require('http')
+const app = express()
+const TOKEN = 'L+XlfDF+f2WQ+UrLEw1bfY5w1K5wly/29W4yb57PQ8aAgSq3ammc3bT4dRf7dDxg6XyjmNQIqZFQcWKoXwWzxM4BvgsMnvjimq1STYO016nBhxJ9uRGjFL7CkyjDmt5p/3ZXWBBhhz9PQKRQ9xQfAQdB04t89/1O/w1cDnyilFU=';
+const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message';
+const LINE_HEADER = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${TOKEN}`
+};
+app.get('/linebot', async (req, res, next) => {
+  console.log(req.body)
+  if (req.body.events[0].message.type !== 'text') {
+    return;
+  }
+  reply(req.body);
+})
+
+const reply = (bodyResponse) => {
+  return request({
+    method: `POST`,
+    uri: `${LINE_MESSAGING_API}/reply`,
+    headers: LINE_HEADER,
+    body: JSON.stringify({
+      replyToken: bodyResponse.events[0].replyToken,
+      messages: [
+        {
+          type: `text`,
+          text: bodyResponse.events[0].message.text
+        }
+	  ]
+    })
+  });
+};
+
+// app.get('/', async (req, res, next) => {
+//   console.log({ auth: req.header('authorization') })
+//   res.set('token', 'test');
+//   res.status(200).send('Hello Express')
+// })
+// app.get('/request', async (req, res, next) => {
+//   http.get({
+//     hostname: 'localhost',
+//     port: 3000,
+//     path: '/',
+//     agent: false,  
+//     headers: {
+//       'authorization': 'Bearer test',
+//     }
+//   }, (response) => {
+
+//     res.status(200).send(response.headers.token);
+//   });
+// })
+
+app.listen(3000, () => console.log('Server is running on port 3000'))
