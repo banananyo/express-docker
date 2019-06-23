1f7d441664c3959a1dfb5527a2adb189f651ecf9
wadjakorn <wadjakorn@dollarsign.co>
2019-06-23 22:54:26 +0700

    change port

--- app.js
+++ app.js
@@ -52,4 +52,4 @@ const reply = (bodyResponse) => {
 //   });
 // })
 
-app.listen(3000, () => console.log('Server is running on port 3000'))
+app.listen(80, () => console.log('Server is running on port 80'))
