a9b96a3ad0f50c23402cf20014f5dad84c08d579
wadjakorn <wadjakorn@dollarsign.co>
2019-06-27 02:26:38 +0700

    add push method

--- app.js
+++ app.js
@@ -62,7 +62,27 @@ const push = (to, messages, notificationDisabled, type) => {
     // console.log(response);
   })
   .catch(function (error) {
-    console.log(error);
+    // Error 😨
+    if (error.response) {
+      /*
+       * The request was made and the server responded with a
+       * status code that falls out of the range of 2xx
+       */
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+        /*
+        * The request was made but no response was received, `error.request`
+        * is an instance of XMLHttpRequest in the browser and an instance
+        * of http.ClientRequest in Node.js
+        */
+        console.log(error.request);
+    } else {
+        // Something happened in setting up the request and triggered an Error
+        console.log('Error', error.message);
+    }
+    console.log(error.config);
   });
 }
 
@@ -84,7 +104,27 @@ const reply = (bodyResponse) => {
     // console.log(response);
   })
   .catch(function (error) {
-    console.log(error);
+    // Error 😨
+    if (error.response) {
+      /*
+       * The request was made and the server responded with a
+       * status code that falls out of the range of 2xx
+       */
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+        /*
+        * The request was made but no response was received, `error.request`
+        * is an instance of XMLHttpRequest in the browser and an instance
+        * of http.ClientRequest in Node.js
+        */
+        console.log(error.request);
+    } else {
+        // Something happened in setting up the request and triggered an Error
+        console.log('Error', error.message);
+    }
+    console.log(error.config);
   });
 
 };
